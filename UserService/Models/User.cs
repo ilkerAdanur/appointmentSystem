9801cdca83dc958729115472d7576481b3948215
ilkerAdanur <adanurilker@gmail.com>
2026-06-24 10:25:15 +0300

    feat: add UserService project

---
+++ UserService/Models/User.cs
@@ -0,0 +1,14 @@
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+
+    public User(int id, string name, string email)
+    {
+        Id = id;
+        Name = name;
+        Email = email;
+    }
+}
---
+++ UserService/Program.cs
@@ -0,0 +1,45 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference(); 
+}
+
+app.UseHttpsRedirection();
+
+var Users = new List<User>
+{
+    new ( 1,"Alice", "alice@example.com" ),
+    new ( 2,"Bob", "bob@example.com" )
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
