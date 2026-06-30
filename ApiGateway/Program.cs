20e5e0340d4a4c92df742c9f14d9974795cde4aa
ilkerAdanur <adanurilker@gmail.com>
2026-06-30 18:45:48 +0300

    Add JWT authentication to API Gateway

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,21 +1,59 @@
+using System.Text;
+using ApiGatewayService.Configuration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
-    .WriteTo.File("Logs/user-service-.log", rollingInterval: RollingInterval.Day)
+    .WriteTo.File("Logs/api-gateway-.log", rollingInterval: RollingInterval.Day)
     .CreateLogger();
 
 builder.Host.UseSerilog();
 
 builder.Services.AddHealthChecks();
+builder.Services.AddAuthorization();
+
+var jwtSettings = builder.Configuration
+    .GetSection("Jwt")
+    .Get<JwtSettings>();
+    
 builder.Services.AddOpenApi();
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            ValidIssuer = jwtSettings!.Issuer,
+            ValidAudience = jwtSettings.Audience,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(jwtSettings.Key))
+        };
+    });
+
+
+
 var app = builder.Build();
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapHealthChecks("/health");
+app.MapReverseProxy();
+
+
+
+
 
 if (app.Environment.IsDevelopment())
 {
